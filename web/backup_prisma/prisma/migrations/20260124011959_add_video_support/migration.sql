86561ff7080918635eddc44d082d47475a39c6b5
carloska24 <carloska24@gmail.com>
2026-02-01 01:06:22 -0300

    refactor: flatten project structure, move web app to root

--- web/backup_prisma/prisma/migrations/20260124011959_add_video_support/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "MediaType" AS ENUM ('IMAGE', 'VIDEO');
-
--- AlterTable
-ALTER TABLE "portfolio_items" ADD COLUMN     "duration" INTEGER DEFAULT 5000,
-ADD COLUMN     "type" "MediaType" NOT NULL DEFAULT 'IMAGE',
-ADD COLUMN     "videoUrl" TEXT;
